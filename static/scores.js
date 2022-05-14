c77c46a8a0422450f99ab7525655f13ab75ffc48
karenEdwardsVM <karen.edwardsvm@gmail.com>
2022-05-13 22:09:45 -0600

    sorted scores, and fixed the trail info

--- static/scores.js
+++ static/scores.js
@@ -1,18 +1,17 @@
 window.onload = async () => {
-  const scores = await getScores();
-  console.log(scores);
-  for (const u in scores) {
+  const scores = await getScores(),
+        sa = Object.entries(scores).sort((a, b) => (b.native + b["non-native"]) - (a.native + a["non-native"]));
+  sa.sort((a, b) => b - a);
+  for (const u of sa) {
     let sDiv = dca('div');
     sDiv.setAttribute("id", "score-div");
     ext(ge('counts'), sDiv);
-    let nat = messageBox(`<div>${u}:</div>
-                          <div>native: ${scores[u].native}</div>
-                          <div>non-native: ${scores[u]['non-native']}</div>`)
+    let nat = messageBox(`<div>${u[0]}:</div>
+                          <div>native: ${u[1].native}</div>
+                          <div>non-native: ${u[1]['non-native']}</div>`)
     nat.style.margin = '2ch';
     nat.style.padding = '1ch';
     nat.style.border = '1px solid black';
     add(ge('score-div'), nat); 
-    //let notNat = messageBox('non-native: ' + scores[u]['non-native']);
-    //add(ge('score-div'), notNat); 
   }
 };
